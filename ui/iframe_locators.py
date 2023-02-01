a67796d768eac37fafd9bdf75bf57c9ac5920576
cristiangarciavd <cristian.garcia.vd@gmail.com>
2023-01-31 21:57:27 -0300

    Create iframe_locators.py

---
+++ ui/iframe_locators.py
@@ -0,0 +1,12 @@
+"""Module for iFrame locators
+"""
+from selenium.webdriver.common.by import By
+
+class IFrame:
+    """iFrame locators
+    """
+    EXTERNAL_I_FRAME = (By.CSS_SELECTOR, 'iframe[class*="plunker"]')
+
+    INTERNAL_I_FRAME = (By.XPATH, '//iframe[@id="preview"]')
+
+    DOM_ELEMENT = (By.CSS_SELECTOR, 'h1')
